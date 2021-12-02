0299f5bf1de50de248e713ef0b5467c14a80d5fb
sorrow <71972762+inadvertently@users.noreply.github.com>
2021-12-01 21:54:34 -0500

    Update var&if.js

--- javascript_prog/var&if.js
+++ javascript_prog/var&if.js
@@ -4,20 +4,22 @@ String.prototype.interpole = function () {
         txt = txt.replace(/{var}/, arguments[c]);
         c++;
     }
-    return txt;
+    return txt; //func will return our vars in text
 };
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max); //func to recieve a random int for the var age
 }
 
-var age = getRandomInt(35);
+var age = getRandomInt(35); //bringing in the func to choose int <=35
+
+//setting our vars, We could just print this but setting it this way gave me a further understanding
 var pg13 = "You can see the pg-13 movie";
 var ratedR = "You can see rated r movies";
 var ratedG = "You can see a rated G movie";
 console.log("Your current age is {var}".interpole(age));
 
-if (age>= 13 && age <= 17)
+if (age>= 13 && age <= 17) //these are self explanitory
   console.log(pg13);
 if (age > 17)
   console.log(ratedR);
